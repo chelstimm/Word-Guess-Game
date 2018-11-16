c8ebaf63d24b6282a49d65321fb5d268f8c804a9
Zach Timmerman <zachtimmerman@Zachs-MacBook-Pro.local>
2018-11-16 15:11:52 -0700

    Fixed kinks and simplified code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,85 +5,99 @@ var reindeerNames = ["dasher", "dancer", "vixen", "comet", "cupid", "blitzen", "
 var reindeerRandom = [Math.floor(Math.random() * reindeerNames.length)];
 // Word that gets chosen//
 var chosenWord = reindeerNames[reindeerRandom];
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
 //Users guesses//
-var rightWord = [];
-var wrongWord = [];
 var blanks = [];
+var wrongLetter = [];
+
 //Tracking Guesses//
 var wins = 0;
 var losses = 0;
 //Declare global variable for the player guesses remaining//
-var remainingGuesses = 10;
-var winCount = 0;
+var guessesLeft = 10;
 
-//Write current status of wins, losses, and guesses//
-
-var docFillblanks = document.getElementsByClassName('blanks');
-var docRightGuess = document.getElementsByClassName('wins');
-var docWrongGuess = document.getElementsByClassName('losses');
-var docRemainingGuesses = document.getElementsByClassName('guessLeft');
-var docCounterLoss = document.getElementsByClassName('lossCounter');
+console.log(chosenWord)
 
 //Generate word length with underscores in place of word letters//
-var fillBlanks = function () {
-    for (var i = 0; i < chosenWord.length; i++) {
-        blanks.push('_');
-    }
-    return blanks;
+
+for (var i = 0; i < chosenWord.length; i++) {
+    blanks.push("_");
 }
+//Push to document//
+document.getElementById("myGuesses").innerHTML = guessesLeft;
+document.getElementById("blanks").innerHTML = blanks.join(" ");
+document.getElementById("myWins").innerHTML = wins;
+document.getElementById("wrongLetter").innerHTML = wrongLetter;
 
-  if (docFillblanks[0].innerHTML = fillBlanks().join(' '));
 
-document.addEventListener('keypress', function (event) {
-    var keyword = String.fromCharCode(event.keyCode);
+//Start Game//
+document.onkeyup = function (event) {
 
-    if (chosenWord.indexOf(keyword) > -1) {
-        rightWord.push(keyword);
+    var keyPress = event.key.toLowerCase();
+    //Documents keypress//
+    if (guessesLeft >= 1) {
+        if (alphabet.indexOf(keyPress) != -1) {
 
-        blanks[chosenWord.indexOf(keyword)] = keyword;
-        docFillblanks[0].innerHTML = blanks.join(' ');
-        docRightGuess[0].innerHTML = winCount++;
-//Alert when user has guess all letters of reindeer name//
-        var fullName = rightWord.join('');
-         if (fullName === chosenWord) { 
-            alert("You Win!");
-            alert("Press another letter key to restart game.")
-         }
+            if (chosenWord.indexOf(keyPress) != -1) {
+                console.log(keyPress);
+
+                for (var j = 0; j < chosenWord.length; j++) {
+                    //If kepress matches letter in word, fill blank, console log, and add wins by one//
+                    if (chosenWord[j] === keyPress) {
+                        blanks[j] = chosenWord[j];
+                        wins++;
+                        document.getElementById("blanks").innerHTML = blanks.join(" ");
+                        document.getElementById("myWins").innerHTML = wins;
+
+                    }
+
+                }
+                //If keypress does not match, document wrong letter guessed and decrease guesses left by one//
+            } else if (wrongLetter.indexOf(keyPress) === -1) {
+                wrongLetter.push(keyPress);
+                guessesLeft = guessesLeft - 1;
+                document.getElementById("wrongLetter").innerHTML = wrongLetter;
+                document.getElementById("blanks").innerHTML = blanks.join(" ");
+                document.getElementById("myGuesses").innerHTML = guessesLeft;
+
+            }
+            //If all letters of the chosen word have been guessed then you win//
+            if (blanks.join("") === chosenWord) {
+                alert("You Win!");
+                wins = wins + 1
+                document.getElementById("myWins").innerHTML = wins;
+                resetGame();
+            }
         }
-else if (wrongWord.push(keyword)); {
-docWrongGuess[0].innerHTML = wrongWord;
-remainingGuesses--;
-docRemainingGuesses[0].innerHTML = remainingGuesses;
-}
-//Method to reset all variables to start game over//
+        //If you haven't guessed chosen word and ran out of guesses, you lose//
+        else {
+            guessesLeft = guessesLeft + 1
+            document.getElementById("myGuesses").innerHTML = guessesLeft;
+            alert("You lose. Try again!");
+            resetGame();
+        }
+        //Function to clear everything and restart game//
+        function resetGame() {
 
-function restart() {
-    
-    reindeerRandom = [Math.floor(Math.random() * reindeerNames.length)];
-    chosenWord = reindeerNames[reindeerRandom];    
-    docFillblanks[0].innerHTML = blanks.join('');
-    docRightGuess[0].innerHTML = winCount++;
+            wrongLetter = [];
+            guessesLeft = 10;
+            losses = 0
+            wins = 0
+            blanks = [];
+            var reindeerRandom = [Math.floor(Math.random() * reindeerNames.length)];
+            chosenWord = reindeerNames[reindeerRandom];
+            for (var i = 0; i < chosenWord.length; i++) {
+                blanks.push("_");
+            }
+            console.log(chosenWord)
 
-    docWrongGuess[0].innerHTML = wrongWord;
-    fullName = rightWord.join('');
-    remainingGuesses = 10;
-    winCount = 0;
-    rightWord = [];
-    wrongWord = [];
-    blanks = [];
-    wins = 0;
-    losses = 0;
-}
-if (fullName === chosenWord) {
-    restart()
-    fillBlanks();
-    return;
-}
-else if (remainingGuesses < 1) {
-    restart()
-    fillBlanks();
-    return;
-}
-  
-});
+            document.getElementById("myGuesses").innerHTML = guessesLeft;
+            document.getElementById("blanks").innerHTML = blanks.join(" ");
+            document.getElementById("myWins").innerHTML = wins;
+            document.getElementById("wrongLetter").innerHTML = wrongLetter;
 
+
+        }
+
+    }
+}  
